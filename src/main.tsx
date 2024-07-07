56fd392d8bb08e733243f73c46aa795be25bf285
Juan basalo <todopinionweb@gmail.com>
2024-07-07 11:46:11 -0500

    Try a clean deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './styles.css';
+import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import { ouiChefTheme } from './ouiChefTheme';
 import { BrowserRouter } from "react-router-dom";
 import { QuotationsWebApp } from './QuotationsWebApp';
 
@@ -11,10 +13,12 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
+      <ThemeProvider theme={ouiChefTheme}>
         <BrowserRouter basename='/quotations-web/'>
           <CssBaseline />
           <QuotationsWebApp />
         </BrowserRouter>
+      </ThemeProvider>
     </React.StrictMode>
   );
 } else {
